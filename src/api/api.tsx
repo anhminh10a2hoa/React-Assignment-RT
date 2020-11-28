20aa5bcf89be326cd77893eed0d08c34732389a7
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-11-28 23:51:37 +0200

    save data in ram

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 let listOfProductsAndManufacturer: any = {};
-let listOfProducts: any = [];
+let listOfProducts: any[] = [];
+let listOfProductsByCategory: any = {};
+
 export async function loadAllManufacturers(){
   const manufacturers = ["abiplos", "derp", "nouke", "reps", "xoon"];
   for(let m of manufacturers){
@@ -24,28 +26,37 @@ export async function loadAllManufacturers(){
 }
 
 export async function getAllProductsByCategory(category: string){
-  let data = await axios.get(`https://bad-api-assignment.reaktor.com/products/${category}`, {
-    headers: {
-      'x-force-error-mode': 'all'
-    }
-  })
-  data.data.map((p: any, i:number) => {
-    let itemManufacturer = listOfProductsAndManufacturer[p.manufacturer];
-    let datapayload = "";
-    let status = [];
-    datapayload = itemManufacturer.find(
-      (val: any) => val.id === p.id.toUpperCase()
-    )["DATAPAYLOAD"];
-    status = datapayload.split('INSTOCKVALUE>');
-    // listOfProducts.push(<Item key={p.id} {...p} status={status[1].replace("</", "")} />)
-    listOfProducts.push({
-      id: p.id,
-      name: p.name,
-      color: p.color,
-      price: p.price,
-      manufacturer: p.manufacturer,
-      status: status[1].replace("</", "")
+  if(listOfProductsByCategory[category]){
+    return listOfProductsByCategory[category]
+  }
+  else {
+    listOfProducts = [];
+    let data = await axios.get(`https://bad-api-assignment.reaktor.com/products/${category}`, {
+      headers: {
+        'x-force-error-mode': 'all'
+      }
     })
-  })
-  return listOfProducts
+    data.data.map((p: any) => {
+      let itemManufacturer = listOfProductsAndManufacturer[p.manufacturer];
+      let datapayload = "";
+      let status = [];
+      datapayload = itemManufacturer.find(
+        (val: any) => val.id === p.id.toUpperCase()
+      )["DATAPAYLOAD"];
+      status = datapayload.split('INSTOCKVALUE>');
+      // listOfProducts.push(<Item key={p.id} {...p} status={status[1].replace("</", "")} />)
+      listOfProducts.push({
+        id: p.id,
+        name: p.name,
+        color: p.color,
+        price: p.price,
+        manufacturer: p.manufacturer,
+        status: status[1].replace("</", "")
+      })
+    })
+    listOfProductsByCategory = Object.assign(listOfProductsByCategory, {
+      [category]: listOfProducts
+    })
+    return listOfProductsByCategory[category]
+  }
 }
